50ed60d93e6a58822d348a887dfd5ac71a88b4a8
Stephane Hamel <koydas@hotmail.com>
2016-09-08 13:18:06 -0400

    baisser le power + faire une barre verte temporaire

---
+++ Assets/Scripts/UI/PowerFilling.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PowerFilling : MonoBehaviour {
+	private Scrollbar _powerBar;
+	private RectTransform _rectTransform;
+
+	void Start () {
+
+		var scrollBars = FindObjectsOfType<Scrollbar>();
+
+		foreach (var scrollBar in scrollBars)
+		{
+			if (scrollBar.name == "Power") {
+				_powerBar = scrollBar;
+			}
+		}
+
+		_rectTransform = GetComponent<RectTransform> ();
+	}
+	
+	void Update () {
+		_rectTransform.sizeDelta = new Vector2 (_rectTransform.sizeDelta.x, _powerBar.value * 157);
+	}
+}
