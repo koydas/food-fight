c6d659e89088ce34ba58f76ca624ea48b79eaa4d
Stephane Hamel <koydas@hotmail.com>
2016-09-19 20:40:36 -0400

    Basic breakable object added

---
+++ Assets/Scripts/Obstacles/Breakable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Obstacles
+{
+    public class Breakable : MonoBehaviour
+    {
+        public int NumberOfHitToBreak = 3;
+
+        public void OnCollisionEnter2D(Collision2D coll)
+        {
+            if (NumberOfHitToBreak <= 0)
+            {
+                Destroy(gameObject);
+            }
+
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            switch (NumberOfHitToBreak)
+            {
+                case 2:
+                    spriteRenderer.color = Color.yellow;
+
+                    break;
+                case 1:
+                    spriteRenderer.color = Color.red;
+                    break;
+                case 0:
+                    Destroy(gameObject);
+                    break;
+            }
+
+            NumberOfHitToBreak--;
+        }
+    }
+}
