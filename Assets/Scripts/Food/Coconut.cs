a181cdd132ad06b90482cb73c8dccc7feb6f6058
Stephane Hamel <koydas@hotmail.com>
2016-10-07 10:14:02 -0400

    Coconut added

---
+++ Assets/Scripts/Food/Coconut.cs
@@ -0,0 +1,48 @@
+﻿using Assets.Scripts.Obstacles;
+using UnityEngine;
+
+namespace Assets.Scripts.Food
+{
+    public class Coconut : Food
+    {
+
+        public override string Title
+        {
+            get { return "Coconut"; }
+        }
+
+        public override string Text
+        {
+            get { return "Description to write."; }
+        }
+        
+        public override Sprite Image
+        {
+            get { return GetComponent<SpriteRenderer>().sprite; }
+        }
+
+        public override EnumFood EnumFood
+        {
+            get
+            {
+                return EnumFood.Coconut;
+            }
+            set { }
+        }
+
+        public override void OnCollisionEnter2D(Collision2D coll)
+        {
+            base.OnCollisionEnter2D(coll);
+
+            if (coll.gameObject.GetComponent<Breakable>())
+            {
+                Destroy(coll.gameObject);
+            }
+        }
+
+        protected override void Rotation()
+        {
+            transform.Rotate(new Vector3(0, 0, 4.5f));
+        }
+    }
+}
--- Assets/Scripts/Food/EnumFood.cs
+++ Assets/Scripts/Food/EnumFood.cs
@@ -3,6 +3,7 @@
     public enum EnumFood
     {
         Blueberry,
+        Coconut,
         FlambeePudding,
         HotPepper,
         Jello,
