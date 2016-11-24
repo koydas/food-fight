e657ba21c0e3a65f967b514488d31ec639948bab
DESKTOP-GGMJJ7H\Stephane <koydas@hotmail.com>
2016-11-23 21:24:59 -0500

    Destroyer walls now invisible and destroy everything on hit !!!!!

---
+++ Assets/Scripts/Obstacles/Destroyer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Obstacles
+{
+    public class Destroyer : MonoBehaviour
+    {
+        public void OnCollisionEnter2D(Collision2D coll)
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
